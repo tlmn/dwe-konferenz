d16533a40986194cbc64c576f779be4a54a45872
tlmn <mail@tlmn.io>
2022-04-10 18:30:27 +0200

    add container config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,16 @@ module.exports = {
       purple: "#811C62",
       yellow: "#FDD816",
     },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     fontFamily: {
       sans: ["Suisse Intl", ...defaultTheme.fontFamily.sans],
     },
