69235d5778bceb3e18ae5146f64895fb8633c225
tlmn <mail@tlmn.io>
2022-04-10 19:02:33 +0200

    prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     screens: {
