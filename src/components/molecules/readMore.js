f8ca4680b9c145e633c883644fab3cf1b3805de4
tlmn <mail@tlmn.io>
2022-04-10 19:02:48 +0200

    add readMore atom

---
+++ src/components/molecules/readMore.js
@@ -0,0 +1,18 @@
+import clsx from "clsx";
+import React, { useState } from "react";
+
+const ReadMore = ({ children, className }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <div className={clsx(!isOpen ? "hidden" : "block")}>{children}</div>
+      <button
+        onClick={() => setIsOpen((prev) => !prev)}
+        className={clsx("button", className)}
+      >
+        {!isOpen ? `Mehr lesen` : `Weniger anzeigen`}
+      </button>
+    </>
+  );
+};
+export default ReadMore;
