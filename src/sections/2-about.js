fc46d89a937fcb91a429080524c67951ec37512c
tlmn <mail@tlmn.io>
2022-04-10 19:27:10 +0200

    style

--- src/sections/2-about.js
+++ src/sections/2-about.js
@@ -3,7 +3,7 @@ import Section from "@/components/molecules/section";
 
 const Register = () => (
   <Section className="bg-purple text-yellow">
-    <ReadMore>Das ist die ReadMore-Komponente.</ReadMore>
+    <ReadMore className="text-purple bg-yellow">Das ist die ReadMore-Komponente.</ReadMore>
   </Section>
 );
 export default Register;
