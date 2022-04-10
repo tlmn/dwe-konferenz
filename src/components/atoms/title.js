a62aafb91526529b0e314ad2ca123837fa02ad0d
tlmn <mail@tlmn.io>
2022-04-10 21:08:28 +0200

    style

--- src/components/atoms/title.js
+++ src/components/atoms/title.js
@@ -2,10 +2,10 @@ import clsx from "clsx";
 import React from "react";
 
 const Title = ({ children, className = "", isBig = false }) => (
-  <div className="text-center my-4 -rotate-[3deg]">
+  <div className="text-center mt-4 mb-6 -rotate-[3deg]">
     <h3
       className={clsx(
-        "py-1 px-2 mx-auto font-bold inline leading-relaxed",
+        "py-1 px-2 mx-auto font-bold inline leading-none",
         isBig ? "text-5xl md:text-6xl" : "text-3xl md:text-4xl",
         className
       )}
