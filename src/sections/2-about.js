504d9ef3598be9f498d221e52ee9a8f70f9cc5cd
tlmn <mail@tlmn.io>
2022-04-10 19:40:06 +0200

    add text

--- src/sections/2-about.js
+++ src/sections/2-about.js
@@ -1,9 +1,22 @@
+import Title from "@/components/atoms/title";
 import ReadMore from "@/components/molecules/readMore";
 import Section from "@/components/molecules/section";
 
 const Register = () => (
-  <Section className="bg-purple text-yellow">
-    <ReadMore className="text-purple bg-yellow">Das ist die ReadMore-Komponente.</ReadMore>
+  <Section className="bg-yellow text-purple">
+    <Title className="text-yellow bg-purple" isBig>Zeit zu reden</Title>
+    <div>
+      Es wird Zeit dass wir miteinander reden. Am Himmelfahrtwochenende (27.-29.
+      Mai 2022) wird eine große Konferenz rund um die Themen Enteignung &amp;
+      Vergesellschaftung von Immobilienkonzernen in den Räumen der TU Berlin
+      stattfinden. In Kooperation mit dem Asta der TU Berlin und der
+      Rosa-Luxemburg Stiftung wollen wir mit dieser Konferenz Alternativen zum
+      profitgetriebenen Wohnwahnsinn starkmachen. Folgende Themen und
+      Fragestellungen wollen wir uns widmen:
+    </div>
+    <ReadMore className="text-yellow bg-purple">
+      Das ist die ReadMore-Komponente.
+    </ReadMore>
   </Section>
 );
 export default Register;
