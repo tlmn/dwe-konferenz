b1e8200c07d70e5a17ffdb9ceddc414c24d52230
tlmn <mail@tlmn.io>
2022-04-10 19:39:50 +0200

    add isBig prop

--- src/components/atoms/title.js
+++ src/components/atoms/title.js
@@ -1,11 +1,12 @@
 import clsx from "clsx";
 import React from "react";
 
-const Title = ({ children, className = "" }) => (
+const Title = ({ children, className = "", isBig = false }) => (
   <div className="text-center my-4 -rotate-[3deg]">
     <h3
       className={clsx(
-        "py-1 px-2 mx-auto text-3xl md:text-4xl font-bold inline leading-relaxed",
+        "py-1 px-2 mx-auto font-bold inline leading-relaxed",
+        isBig ? "text-5xl md:text-6xl" : "text-3xl md:text-4xl",
         className
       )}
       style={{ WebkitBoxDecorationBreak: "clone" }}
