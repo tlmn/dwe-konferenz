06c9bd435de26a5b1f74353b0351c9b03468efd8
tlmn <mail@tlmn.io>
2022-04-10 21:08:08 +0200

    style Carousel

--- src/components/atoms/carousel.js
+++ src/components/atoms/carousel.js
@@ -1,19 +1,22 @@
-import React from "react"
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import { Carousel } from "react-responsive-carousel"
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 const DWECarousel = ({ children }) => {
   return (
-    <Carousel
-      showIndicators={false}
-      interval={2000}
-      showArrows={false}
-      showStatus={false}
-      autoPlay={true}
-    >
-      {children}
-    </Carousel>
-  )
-}
+    <div className="my-5 col-span-full">
+      <Carousel
+        showIndicators={false}
+        interval={2000}
+        showArrows={false}
+        showStatus={false}
+        autoPlay={true}
+        showThumbs={false}
+      >
+        {children}
+      </Carousel>
+    </div>
+  );
+};
 
-export default DWECarousel
+export default DWECarousel;
