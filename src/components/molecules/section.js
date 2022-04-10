5ca362784e51e47a2a28f3ef3b71b716a428d156
tlmn <mail@tlmn.io>
2022-04-10 19:33:08 +0200

    add arrow dynamically

--- src/components/molecules/section.js
+++ src/components/molecules/section.js
@@ -1,9 +1,27 @@
 import clsx from "clsx";
 
-const Section = ({ children, className = "", hasArrow = false }) => (
-  <div className={clsx("w-full py-3", className)}>
-    <div className="container flex flex-col h-full">{children}</div>
-  </div>
-);
+const Section = ({ children, className = "", hasArrow = false }) => {
+  const Content = () => (
+    <div className={clsx("w-full py-5", hasArrow && "hasArrow", className)}>
+      <div className="container flex flex-col h-full">{children}</div>
+    </div>
+  );
+
+  return (
+    <>
+      {hasArrow ? (
+        <div
+          className={clsx(
+            className.includes("bg-yellow") ? "bg-purple" : "bg-yellow"
+          )}
+        >
+          <Content />
+        </div>
+      ) : (
+        <Content />
+      )}
+    </>
+  );
+};
 
 export default Section;
