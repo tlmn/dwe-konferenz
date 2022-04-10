a12292de29690b2a8f44655ba3567ab551d2ad1f
tlmn <mail@tlmn.io>
2022-04-10 19:45:04 +0200

    check if is Array

--- src/components/molecules/programmeDay.js
+++ src/components/molecules/programmeDay.js
@@ -1,20 +1,25 @@
-import React from "react"
-import Title from "../atoms/title"
+import React from "react";
+import Title from "../atoms/title";
 
 const ProgrammeDay = ({ day, children }) => {
   return (
     <div className="text-left">
       <Title className="text-purple bg-yellow">{day}</Title>
-      {children.map((child, index) => (
-        <div key={index}>
-          {child}
-          {children.length !== index + 1 && (
-            <hr style={{ borderTop: `3px dotted #FDD816` }} className="my-2" />
-          )}
-        </div>
-      ))}
+      {Array.isArray(children)
+        ? children.map((child, index) => (
+            <div key={index}>
+              {child}
+              {children.length !== index + 1 && (
+                <hr
+                  style={{ borderTop: `3px dotted #FDD816` }}
+                  className="my-2"
+                />
+              )}
+            </div>
+          ))
+        : children}
     </div>
-  )
-}
+  );
+};
 
-export default ProgrammeDay
+export default ProgrammeDay;
