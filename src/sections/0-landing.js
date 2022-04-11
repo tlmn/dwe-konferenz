eeb81b23db206c61cef2f2b0ad3b3507662bd5cb
tlmn <mail@tlmn.io>
2022-04-11 06:57:21 +0200

    add hero

--- src/sections/0-landing.js
+++ src/sections/0-landing.js
@@ -1,11 +1,12 @@
 import LogoWordMark from "@/assets/icons/dwe/logoWordMark";
+import LandingHero from "@/assets/landingHero";
 import Section from "@/components/molecules/section";
 
 const Landing = () => (
   <Section className="bg-yellow text-purple h-screen">
     <LogoWordMark />
     <div className="flex-1 flex items-center justify-center">
-      <h1>Hier kommt der Claim rein</h1>
+      <LandingHero className="w-full md:w-[40%]" />
     </div>
   </Section>
 );
