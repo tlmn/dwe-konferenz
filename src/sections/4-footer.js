7526edbdae601a34e483ad12b513ec7ab8c4a700
tlmn <mail@tlmn.io>
2022-04-11 06:53:12 +0200

    styling

--- src/sections/4-footer.js
+++ src/sections/4-footer.js
@@ -4,29 +4,29 @@ import Section from "@/components/molecules/section";
 
 const Footer = () => (
   <Section className="bg-yellow text-purple">
-    <ul className="flex justify-around">
+    <ul className="flex justify-around flex-wrap">
       <li>
-        <a href="" className="button bg-purple text-yellow">
+        <a href="" className="button bg-purple text-yellow my-1 block">
           Spenden
         </a>
       </li>
       <li>
-        <a href="" className="button bg-purple text-yellow">
+        <a href="" className="button bg-purple text-yellow my-1 block">
           Mitmachen
         </a>
       </li>
       <li>
-        <a href="" className="button bg-purple text-yellow">
+        <a href="" className="button bg-purple text-yellow my-1 block">
           Impressum
         </a>
       </li>
       <li>
-        <a href="" className="button bg-purple text-yellow">
+        <a href="" className="button bg-purple text-yellow my-1 block">
           Kontakt
         </a>
       </li>
     </ul>
-    <ul className="mt-6 flex justify-around">
+    <ul className="mt-6 flex justify-around flex-wrap">
       <li>
         <a href="">
           <LogoWordMark />
