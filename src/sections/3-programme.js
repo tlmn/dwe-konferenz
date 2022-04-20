e8d6fbdb5d807ca2e7379de9e486d789f9d5ee55
tlmn <mail@tlmn.io>
2022-04-20 23:34:35 +0200

    prettier

--- src/sections/3-programme.js
+++ src/sections/3-programme.js
@@ -10,24 +10,40 @@ const Programme = () => (
       Programm
     </Title>
     <ProgrammeDay day="Freitag, 27. Mai">
-      <ProgrammeItem title="Was ist: Finanzialisierung des Wohnens? (Crashkurse 1)" time="17:30 - 18:30">
+      <ProgrammeItem
+        title="Was ist: Finanzialisierung des Wohnens? (Crashkurse 1)"
+        time="17:30 - 18:30"
+      >
         Ref: Philipp Metzger
       </ProgrammeItem>
-      <ProgrammeItem title="Wohnungsfrage und Wohnungspolitik (Crashkurse 2)" time="17:30 - 18:30">
+      <ProgrammeItem
+        title="Wohnungsfrage und Wohnungspolitik (Crashkurse 2)"
+        time="17:30 - 18:30"
+      >
         Ref: Andrej Holm
       </ProgrammeItem>
-      <ProgrammeItem title="Art. 15 für Nicht-Jurist:innen (Crashkurse 3)" time="17:30 - 18:30">
+      <ProgrammeItem
+        title="Art. 15 für Nicht-Jurist:innen (Crashkurse 3)"
+        time="17:30 - 18:30"
+      >
         Ref: Franziska Drohsel
       </ProgrammeItem>
-    <ProgrammeItem title="Vom Einzelfall zur organisierten Nachbarschaft (Crashkurse 4)" time="17:30 - 18:30">
-       Wie Organisierende Mietrechts- Sozial-Beratungen und Nachbarschaftskampagnen zu erfolgreichen Mieter*Innenkämpfen um Enteignung & Co beitragen können 
-       Ref: Tashy Endres und Georgios Thodos (beide: AG Starthilfe)
+      <ProgrammeItem
+        title="Vom Einzelfall zur organisierten Nachbarschaft (Crashkurse 4)"
+        time="17:30 - 18:30"
+      >
+        Wie Organisierende Mietrechts- Sozial-Beratungen und
+        Nachbarschaftskampagnen zu erfolgreichen Mieter*Innenkämpfen um
+        Enteignung & Co beitragen können Ref: Tashy Endres und Georgios Thodos
+        (beide: AG Starthilfe)
       </ProgrammeItem>
-    <ProgrammeItem title="Auftaktveranstaltung: Der Kampf um Vergesellschaftung im internationalen Kontext" time="19:00 - 21:00">
-       mit Balakrishnan Rajagopal (Video), danach Gespräch mit Andrej Holm, jemanden von DWE und evtl. weiterem internationalen Gast.
-       moderiert von Nina Scholz
-       Livestream
-
+      <ProgrammeItem
+        title="Auftaktveranstaltung: Der Kampf um Vergesellschaftung im internationalen Kontext"
+        time="19:00 - 21:00"
+      >
+        mit Balakrishnan Rajagopal (Video), danach Gespräch mit Andrej Holm,
+        jemanden von DWE und evtl. weiterem internationalen Gast. moderiert von
+        Nina Scholz Livestream
       </ProgrammeItem>
     </ProgrammeDay>
     <ProgrammeDay day="Samstag, 28. Mai">
