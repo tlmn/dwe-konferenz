775a474bbe1bfa0499c0bb4802684566ec667d54
Zoff / Pierre <41169887+Pierre-Zoff@users.noreply.github.com>
2022-04-24 17:19:51 +0200

    Update 3-programme.js
    Text Update SessionTitle und erste Workshop

--- src/sections/3-programme.js
+++ src/sections/3-programme.js
@@ -52,10 +52,31 @@ const Programme = () => (
       </ProgrammeItem>
     </ProgrammeDay>
     <ProgrammeDay day="Samstag, 28. Mai">
-      <ProgrammeItem title="Willkommen" time="Zeit">
-        Crashkurse zu Finanzialisierung (Philipp Metzger), Gentrifizierung und
-        Mietenbewegung (Lisa Vollmer oder Andrej zu seinem Buch), Art. 15 für
-        Nicht-Jurist:innen (Cara Röhner oder Franziska Drohsel)
+      
+      <SessionTitle className="text-yellow">Streitgespräch: Vergesellschaftung jetzt!</SessionTitle>
+
+      <ProgrammeItem title="Follow the money - Der deutsche Immobilienmarkt als Tummelplatz für Oligarchen, Kriminelle und Steuerhinterzieher" time="11-19 Uhr">
+        Christoph Trautvetter (Netzwerk Steuergerechtigkeit, externer Projektleiter „Wem gehört die Stadt?“), Gabriela
+Keller (Correctiv), Moderation: Katalin Gennburg
+ 
+Berlin ist aus Sicht internationaler Kapitalströme zu einem der Zielorte schlechthin avanciert:
+die gebaute Umwelt der Stadt verspricht hohe Renditen und Möglichkeiten zur Spekulation.
+Doch woher kommt das Kapital? Aufwendig verschachtelte Firmen- und
+Beteiligungskonstrukte, die bis in das europäische wie außereuropäische Ausland reichen,
+verschleiern den Ursprung der Kapitalströme und verdecken oftmals, wer die tatsächlich
+wirtschaftlich Berechtigten hinter einer Immobilie sind. Sie ermöglichen so Geldwäsche und
+Steuerhinterziehung. 
+
+Die Veranstaltung geht der Frage nach, welche Methoden der Verschleierung zum Einsatz
+kommen – und welche gesetzgeberischen Schlupflöcher dies überhaupt erst ermöglichen.
+Aufgezeigt werden sollen diese Methoden anhand konkreter Beispiele aus der jüngeren
+Stadtgeschichte Berlins, wobei sich auch der Frage nach der Rolle russischer Oligarchen auf
+dem Berliner Immobilienmarkt zugewandt werden soll.
+
+
+      <SessionTitle className="text-yellow">Themenstrang 1: Vergesellschaftung</SessionTitle>
+
+
       </ProgrammeItem>
       <ProgrammeItem title="Willkommen" time="Zeit">
         Crashkurse zu Finanzialisierung (Philipp Metzger), Gentrifizierung und
