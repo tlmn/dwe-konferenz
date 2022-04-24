6f3013b2b646725b766750b06562557ff1608e86
tlmn <mail@tlmn.io>
2022-04-24 18:24:31 +0200

    set fixed height

--- src/assets/landingHero--desktop.js
+++ src/assets/landingHero--desktop.js
@@ -4,6 +4,8 @@ const LandingHeroDesktop = ({ className }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
+    width="875"
+    height="719"
     viewBox="0 0 875 719"
     className={className}
   >
