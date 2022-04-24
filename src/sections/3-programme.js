d8bdd8c4227a88f1bc4953b1f2e1979095ec543d
Zoff / Pierre <41169887+Pierre-Zoff@users.noreply.github.com>
2022-04-24 17:44:58 +0200

    Update 3-programme.js
    Text Update clean up

--- src/sections/3-programme.js
+++ src/sections/3-programme.js
@@ -60,15 +60,18 @@ const Programme = () => (
 
       <ProgrammeItem
         title="Auftakt: Eröffnungsrede, Begrüßung, Keynote, /Gespräch?"
-        time="10-11 Uhr">
+        time="10-11 Uhr"
+      >
          Prof. Tim Wihl
+      </ProgrammeItem>
 
 
       <SessionTitle className="text-yellow">Themenstrang 1: Vergesellschaftung</SessionTitle>
 
    <ProgrammeItem
       title="Follow the money - Der deutsche Immobilienmarkt als Tummelplatz für Oligarchen, Kriminelle und Steuerhinterzieher"
-      time="11-19 Uhr">
+      time="11-19 Uhr"
+  >
       Christoph Trautvetter (Netzwerk Steuergerechtigkeit, externer Projektleiter „Wem gehört die Stadt?“), Gabriela
 Keller (Correctiv), Moderation: Katalin Gennburg<br>
 Berlin ist aus Sicht internationaler Kapitalströme zu einem der Zielorte schlechthin avanciert:
@@ -87,7 +90,8 @@ dem Berliner Immobilienmarkt zugewandt werden soll.
 
       <ProgrammeItem
         title="Eine Anstalt öffentlichen Rechts - unsere Alternative zu privaten Immobilienkonzernen"
-        time="11-19 Uhr">
+        time="11-19 Uhr"
+      >
         Die Wohnungsbestände großer Immobilienkonzerne in Berlin zu vergesellschaften, d.h. in demokratisch verwaltetes Gemeineigentum zu überführen, ist das Ziel der Kampagne Deutsche Wohnen und Co enteignen. 
 Aber was genau bedeutet das? Wie muss die geplante Anstalt öffentlichen Rechts organisiert sein, um eine gemeinwohlorientierte, funktionierende und demokratische Verwaltung zu ermöglichen? 
   Zu dieser Frage bereitet die Vergesellschaftungs-AG der Kampagne derzeit einen Vorschlag vor.
@@ -99,7 +103,9 @@ Referent*innen: Vergesellschaftungs-AG von DWE
       </ProgrammeItem>
 
       <ProgrammeItem
-        title="Vom Mieter*innen-Protest zur Enteignung? Über das komplizierte Verhältnis von Mieter*innen-Kämpfen und Enteigungs-Kampagne  " time="11-19 Uhr">
+        title="Vom Mieter*innen-Protest zur Enteignung? Über das komplizierte Verhältnis von Mieter*innen-Kämpfen und Enteigungs-Kampagne  "
+        time="11-19 Uhr"
+      >
        Ohne die Mieter*innen-Proteste der Deutsche Wohnen Mieter*innen gegen Mieterhöhungen und Verdrängungen wäre die Enteignungskampagne in Berlin nie möglich gewesen. 
        Die Kampagne selbst weist über die Kämpfe hinaus. Während des Volksentscheids haben sich Kampagne und konkrete Kämpfe immer wieder gegenseitig beflügelt. 
        Ohne Organisierung gibt es also keine Enteignung - und trotzdem bleiben die Aktivist*innen in der Kampagne oft unter sich. 
@@ -109,47 +115,62 @@ Hannes Strobel und Nina Scholz (AG Starthilfe)
 
       <SessionTitle className="text-yellow">Themenstrang 2: Skill-Sharing</SessionTitle>
 
- <ProgrammeItem
+     <ProgrammeItem
     title="Vergesellschaftung - Social Media und soziale Bewegungen"
-    time="11-19 Uhr">
+    time="11-19 Uhr"
+     >
         Zwischen Memes, TikToks und Insta-Stories. Wie läuft die Social Media Arbeit bei Deutsche Wohnen & Co enteignen? Wie entwickeln wir Posts? Wie gelingt ein erfolreiches Community Building und wie gehen wir mit Shitstorms um? Diese und viele weitere Fragen wollen wir zusammen mit euch in diesem Workshop beantworten.
 Wir stellen unsere Arbeitsweise vor, analysieren gemeinsam Posts, sprechen über praktische Tipps und politische Kommunikation in Sozialen Medien. Außerdem wird es praktische Übungen geben, bitte bringt daher wenn möglich einen Laptop mit. <br> 
         Referent*innen: Mareike & Marcel (Social Media AG DWE) 
       </ProgrammeItem>
 
- <ProgrammeItem
+    <ProgrammeItem
     title="Data-driven campaigning oder was Daten alles (nicht) können"
-     time="11-19 Uhr">
+     time="11-19 Uhr"
+   >
         „Kenne Dein Potential“ gilt nicht nur allgemein als Lebensweisheit sondern ist auch für das Organizing unabdingbar. Wer ist unsere Zielgruppe? Wie viele Stimmen können wir in welchem Bezirk erreichen? In welchem Bezirk ist die Zustimmung am größten, in welchem am kleinsten? 
 Zentrale Fragen, die in Plenumsdebatten nach Bauchgefühl oder individuellen Erlebnissen diskutiert werden, können datenbasiert empirisch bestimmt werden, oder? 
 Die Datentaskforce berichtet über Mapping, Statistik und Fallstricke der Kommunikation.
-
       </ProgrammeItem>
 
  <ProgrammeItem
         title="Es wurde schon alles gesagt - nur nicht von (allen) FLINTA"
-        time="11-19 Uhr">
+        time="11-19 Uhr"
+          >
       Was ist eigentlich eine Kernbotschaft? Muss ich auf die Frage antworten, die mir gestellt wurde - oder darf ich einfach auf eine andere antworten? Und wer hilft mir dabei? Ein Workshop für FLINTA rund ums Sprechen in der Öffentlichkeit: Wie Medien- und Pressearbeit bei DWE funktioniert und warum mit Kompliz*innen alles besser ist.
       </ProgrammeItem>
 
-
     </ProgrammeDay>
     <ProgrammeDay day="Sonntag, 29. Mai">
-      <ProgrammeItem title="#Titel" time="11-19 Uhr">
+      
+      <ProgrammeItem
+        title="#Titel"
+        time="11-19 Uhr"
+      >
+        Crashkurse zu Finanzialisierung (Philipp Metzger), Gentrifizierung und
+        Mietenbewegung (Lisa Vollmer oder Andrej zu seinem Buch), Art. 15 für
+        Nicht-Jurist:innen (Cara Röhner oder Franziska Drohsel)
+
+      </ProgrammeItem>
+      <ProgrammeItem
+        title="Willkommen"
+        time="11-19 Uhr"
+      >
+        Crashkurse zu Finanzialisierung (Philipp Metzger), Gentrifizierung und
+        Mietenbewegung (Lisa Vollmer oder Andrej zu seinem Buch), Art. 15 für
+        Nicht-Jurist:innen (Cara Röhner oder Franziska Drohsel)
+
+      </ProgrammeItem>
+      <ProgrammeItem
+        title="Willkommen"
+        time="11-19 Uhr"
+      >
         Crashkurse zu Finanzialisierung (Philipp Metzger), Gentrifizierung und
         Mietenbewegung (Lisa Vollmer oder Andrej zu seinem Buch), Art. 15 für
         Nicht-Jurist:innen (Cara Röhner oder Franziska Drohsel)
       </ProgrammeItem>
-      <ProgrammeItem title="Willkommen" time="11-19 Uhr">
-        Crashkurse zu Finanzialisierung (Philipp Metzger), Gentrifizierung und
-        Mietenbewegung (Lisa Vollmer oder Andrej zu seinem Buch), Art. 15 für
-        Nicht-Jurist:innen (Cara Röhner oder Franziska Drohsel)
-      </ProgrammeItem>
-      <ProgrammeItem title="Willkommen" time="11-19 Uhr">
-        Crashkurse zu Finanzialisierung (Philipp Metzger), Gentrifizierung und
-        Mietenbewegung (Lisa Vollmer oder Andrej zu seinem Buch), Art. 15 für
-        Nicht-Jurist:innen (Cara Röhner oder Franziska Drohsel)
-      </ProgrammeItem>
+
+
     </ProgrammeDay>
     <a href="" target="_blank" className="button bg-yellow text-purple mx-auto">
       Jetzt anmelden!
