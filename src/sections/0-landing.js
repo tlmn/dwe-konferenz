eca2b197ebbe8809ce054a3480edcc0e88f65e46
tlmn <mail@tlmn.io>
2022-04-24 14:24:12 +0200

    set image cover container

--- src/sections/0-landing.js
+++ src/sections/0-landing.js
@@ -1,25 +1,29 @@
 import LogoWordMark from "@/assets/icons/dwe/logoWordMark";
-import LandingHero from "@/assets/landingHero";
+import LandingHeroDesktop from "@/assets/landingHero--desktop";
+import LandingHeroMobile from "@/assets/landingHero--mobile";
 import Button from "@/components/atoms/button";
 import Section from "@/components/molecules/section";
 import Image from "next/image";
 import ImageLanding from "@/public/images/landing.jpg";
 
 const Landing = () => (
-  <Section className="bg-yellow text-purple h-screen">
+  <>
+    <Section className="bg-yellow text-purple h-screen relative">
+      <div className="relative z-10">
+        <div>
+          <Button href="https://www.dwenteignen.de">
+            <LogoWordMark />
+          </Button>
+        </div>
+        <div className="flex items-center justify-center relative">
+          {/* <LandingHeroDesktop className="h-full hidden md:block" />
+          <LandingHeroMobile className="h-full block md:hidden" /> */}
+        </div>
+      </div>
+    </Section>
     <div className="absolute top-0 left-0 h-full w-full z-0 overflow-hidden">
-      <Image src={ImageLanding} alt="" layout="responsive" />
+      <Image src={ImageLanding} alt="" layout="fill" objectFit="cover" />
     </div>
-    <div className="relative z-10">
-      <div>
-        <Button href="https://www.dwenteignen.de">
-          <LogoWordMark />
-        </Button>
-      </div>
-      <div className="flex-1 flex items-center justify-center">
-        <LandingHero className="w-full md:w-[40%]" />
-      </div>
-    </div>
-  </Section>
+  </>
 );
 export default Landing;
