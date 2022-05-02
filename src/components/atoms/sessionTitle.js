3f69cf6097f16629c53433ff7813b2507a21fff9
tlmn <mail@tlmn.io>
2022-05-02 09:02:24 +0200

    fix and prettier

--- src/components/atoms/sessionTitle.js
+++ src/components/atoms/sessionTitle.js
@@ -1,7 +1,9 @@
 import clsx from "clsx";
 
 const SessionTitle = ({ className, children }) => (
-  <h4 className={clsx("uppercase text-center text-2xl sm:text-3xl", className)}>{children}</h4>
+  <h4 className={clsx("uppercase text-center text-2xl sm:text-3xl", className)}>
+    {children}
+  </h4>
 );
 
 export default SessionTitle;
--- src/sections/1-register.js
+++ src/sections/1-register.js
@@ -5,7 +5,11 @@ const Register = () => (
   <Section className="bg-purple text-yellow" hasArrow>
     <div className="flex-1 flex flex-col items-center justify-center">
       <Title className="bg-yellow text-purple">Ich will dabei sein</Title>
-      <a href="https://dwewolke.de/index.php/apps/forms/ymPCk2GbeTi9rnzx" target="_blank" className="button bg-yellow text-purple">
+      <a
+        href="https://dwewolke.de/index.php/apps/forms/ymPCk2GbeTi9rnzx"
+        target="_blank"
+        className="button bg-yellow text-purple"
+      >
         Jetzt anmelden!
       </a>
     </div>
