1489759dab15d26bdb5514ee3cdf52a0dce6829b
tlmn <mail@tlmn.io>
2022-05-12 20:42:11 +0200

    use json

--- src/components/atoms/programmeItem.js
+++ src/components/atoms/programmeItem.js
@@ -1,36 +1,19 @@
 import clsx from "clsx";
 import React, { useState } from "react";
 
-const ProgrammeItem = ({ title, time, children, category }) => {
+const ProgrammeItem = ({ title, time, location, children, category }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <span>{time}</span>
+      <span>{time}, Ort: {location}</span>
       <h3
         className="font-bold text-2xl underline md:no-underline"
         onClick={() => setIsOpen(!isOpen)}
       >
         {title}
       </h3>
-      <span>{category}</span>
-      {Array.isArray(children) ? (
-        children.map((child, index) => {
-          return typeof child === "object" ? (
-            <br key={index} />
-          ) : (
-            <p
-              dangerouslySetInnerHTML={{ __html: child }}
-              className={clsx("text-lg", !isOpen && "hidden", "md:block")}
-              key={index}
-            />
-          );
-        })
-      ) : (
-        <p
-          dangerouslySetInnerHTML={{ __html: children }}
-          className={clsx("text-lg", !isOpen && "hidden", "md:block")}
-        />
-      )}
+      {children}
+      
     </div>
   );
 };
